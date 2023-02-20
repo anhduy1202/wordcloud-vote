ea8c7ccb427b5870a97c97a29a41996bbc4b057c
Daniel Truong <aduy1122@gmail.com>
2023-02-20 03:01:06 -0800

    Prevent route.push when delete

--- frontend/src/components/Polls/SinglePoll.tsx
+++ frontend/src/components/Polls/SinglePoll.tsx
@@ -20,26 +20,28 @@ export const SinglePoll: React.FC<PollProps> = (props) => {
     day: "numeric",
   };
   const formattedDate = date.toLocaleDateString("en-US", option);
-  const goToPoll = () => {
-    router.push(`/poll/${id}`);
-  };
 
   return (
-    <div
-      className="bg-blue-100 p-4 rounded-md cursor-pointer relative md:p-8 my-14"
-      onClick={() => goToPoll}
-    >
-      <p className="font-bold md:text-[1.75rem]"> {title}</p>
-      <p className="text-[0.85rem] border-2 rounded-[2rem] border-btn-important px-2 py-1 absolute top-[-20%] right-[-2%] font-semibold bg-white md:text-[1rem]">
-        {responses.length} Votes
-      </p>
-      <p className="text-[0.75rem] md:text-[1rem]"> {formattedDate} </p>
+    <div className="bg-blue-100 p-4 rounded-md cursor-pointer relative md:p-8 my-14">
+      <div className="" onClick={() => router.push(`/poll/${id}`)}>
+        <p className="font-bold md:text-[1.75rem]"> {title}</p>
+        <p className="text-[0.85rem] border-2 rounded-[2rem] border-btn-important px-2 py-1 absolute top-[-20%] right-[-2%] font-semibold bg-white md:text-[1rem]">
+          {responses.length} Votes
+        </p>
+        <p className="text-[0.75rem] md:text-[1rem]"> {formattedDate} </p>
+      </div>
       <MdOutlineCancel
         size={32}
         className="absolute right-[-3%] top-[-32%] text-black hover:text-red-500"
         onClick={() => setOpen(true)}
       />
-      <DeletePopUp isOpen={isOpen} setOpen={setOpen} id={id} title="Delete this poll?" description="This action can't be undone"/>
+      <DeletePopUp
+        isOpen={isOpen}
+        setOpen={setOpen}
+        id={id}
+        title="Delete this poll?"
+        description="This action can't be undone"
+      />
     </div>
   );
 };
